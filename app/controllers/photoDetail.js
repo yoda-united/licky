ce0aaa9b80a0306a8c7b2f1eb93a9a7b7ff51718
Jongeun Lee <yomybaby@yomybaby.com>
2013-11-22 11:49:34 +0900

    댓글 작성 form 작성시 키보드 및 핸들링

--- app/controllers/photoDetail.js
+++ app/controllers/photoDetail.js
@@ -3,7 +3,78 @@ var args = arguments[0] || {},
 
 var contentItem = photoModel.doDefaultTransform();
 contentItem.template = 'photoItemTemplate';
+contentItem.properties.selectionStyle = Ti.UI.iPhone.ListViewCellSelectionStyle.NONE;
 contentItem.properties.height = 180;
 $.contentSection.setItems([
 	contentItem
 ]);
+
+
+var commentCol = Alloy.createCollection('review');
+commentCol.on('reset',function(col){
+	col.each(function(comment){
+		Ti.APi.info(comment.attributes);
+	});
+});
+// commentCol.fetch({
+	// data : {
+		// photo_id : photoModel.id
+	// }
+// });
+
+// AG.Cloud.Reviews.create({
+    // photo_id: '528c6a3f00de2c0b32001120',
+    // content: 'Good'
+// }, function (e) {
+    // if (e.success) {
+        // var review = e.reviews[0];
+        // alert('Success:\n' +
+            // 'id: ' + review.id + '\n' +
+            // 'rating: ' + review.rating + '\n' +
+            // 'content: ' + review.content + '\n' +
+            // 'updated_at: ' + review.updated_at);
+    // } else {
+        // alert('Error:\n' +
+            // ((e.error && e.message) || JSON.stringify(e)));
+    // }
+// });
+
+$.commentField.addEventListener('focus', function(e) {
+	$.mainWrap.animate({
+		bottom:216,
+		duration : 200
+	});
+});
+$.commentField.addEventListener('blur', function(e) {
+	$.mainWrap.animate({
+		bottom:0,
+		duration : 200
+	});
+});
+
+var doCommentBlur = function(){
+	$.mainWrap.animate({
+		bottom:0,
+		duration : 200
+	});
+	$.commentField.blur();
+};
+
+$.listView.addEventListener('singletap', function(e) {
+	//$.commentField.blur();
+	doCommentBlur();
+});
+
+$.sendBtn.addEventListener('click', function(e) {
+	//alert($.commentField.value);
+	$.commentField.value = '';
+	doCommentBlur();
+});
+
+Ti.App.addEventListener('keyboardframechanged', function(e) {
+	if(e.keyboardFrame.width==320 && e.keyboardFrame.y<400){ //appear
+		$.mainWrap.bottom = e.keyboardFrame.height;
+	}else{ //disappear
+		
+	}
+});
