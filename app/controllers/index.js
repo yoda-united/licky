6573f5d41f91f27f04ddfb5279b9eaa2b3dc6c36
Daehyeon Shin <dahini@gmail.com>
2013-12-29 18:34:21 +0900

    fixed #39 index.xml 의 tabGroup에 id를 지정하면 $.index.open() 부분에서 에러가 나서 수정

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,8 @@
-AG.mainTabGroup = $.index;
-$.index.open();
+// 이 방식으로는  index.xml 의 탭 그룹에 id를 지정할때 에러가 나서 주석 처리 했어요.
+// AG.mainTabGroup = $.index;
+// $.index.open();
+AG.mainTabGroup = $.mainTabGroup;
+AG.mainTabGroup.open();
 // $.getView().add(Alloy.createController('cameraOveray').getView());
 
 
