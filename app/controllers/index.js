f6fdf44a9e713fd62d940cfd9e27654235911813
Daehyeon Shin <dahini@gmail.com>
2013-12-30 19:53:47 +0900

    프랜즈탭 아이콘 디자인하고 적용 resolved #40, see #37.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,4 @@
-// 이 방식으로는  index.xml 의 탭 그룹에 id를 지정할때 에러가 나서 주석 처리 했어요.
+// 최상위 요소는 컨트롤러이름으로 아이디가 지정되는걸 이용하는 방식으로 돌아감 
 AG.mainTabGroup = $.index;
 $.index.open();
 // AG.mainTabGroup = $.mainTabGroup;
