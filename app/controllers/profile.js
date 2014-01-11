dea90b8f05e79c81cb27cc6994e540699a9ee36d
Daehyeon Shin <dahini@gmail.com>
2014-01-11 18:06:53 +0900

    BOG-75 #resolve 프로필에 들어갈 메뉴를 더 정해야 겠음. 너무 횡함.
    Former-commit-id: 0d642a3370c595ee0249aa512f832240bfbf5664

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,6 +1,5 @@
 var args = arguments[0] || {}, user = args.user;
 
-
 if (user) {
 	Ti.API.info("[profile.js] another user");
 } else {
@@ -11,47 +10,17 @@ if (user) {
 	}
 }
 
-// TODO: 스타일로 빼고 싶은데..
-$.photoListC.getView().top = 272.5;	// profileBanner + controlBar's height
-$.photoListC.getView().setBubbleParent(false);
-
-// 컨트롤바를 건드리면 리스트뷰가 꽉찬 화면이랑 프로필 베너가 다 보이는 화면이 전환되게 하는 로직 
-$.controlBar.addEventListener('touchstart', function(e){
-	var offset = $.mainContent.getContentOffset().y;
-	if( offset === 0 ){
-		$.mainContent.setContentOffset({y:212.5});
-		$.mainContent.setScrollingEnabled(false);
-	}else{
-		$.mainContent.setContentOffset({y:0});
-		$.mainContent.setScrollingEnabled(true);
-	}
+$.menuTable.setBubbleParent(false);
+$.foodRow.setTitle(L("ate_foods"));
+$.foodRow.addEventListener('click', function(e) {
+	AG.utils.openController(AG.mainTabGroup.activeTab, "photoList", {
+		// photoModel : photoCol.get(e.itemId) //clicked Model
+	});
 });
-// 리스트뷰가 꽉찬 화면이랑 프로필 베너가 다 보이는 화면이랑 둘 중 하나만 보이는 페이지 느낌이 나도록 하는 로직.
-$.mainContent.addEventListener('dragend', function(e){
-	var offset = $.mainContent.getContentOffset().y;
-	if( offset < 80 ){	// 80 is trigger offset
-		$.mainContent.setContentOffset({y:0});
-		$.mainContent.setScrollingEnabled(true);
-	}else{
-		$.mainContent.setContentOffset({y:212.5});
-		$.mainContent.setScrollingEnabled(false);
-	}
-});
-// 리스트뷰가 화면에 가득 찼을 때는 메인컨탠트뷰 스크롤을 금지 시킨다.
-// $.mainContent.addEventListener('scrollend', function(e){
-	// var offset = $.mainContent.getContentOffset().y;
-	// if( offset === 0 ){
-		// $.mainContent.setScrollingEnabled(true);
-	// }else{
-		// $.mainContent.setScrollingEnabled(false);
-	// }
-	// alert("se");
-// });
 
-
-$.settingDialog.addEventListener('click', function(e){
+$.settingDialog.addEventListener('click', function(e) {
 	// alert(e);
-	if(e.index === 0){
+	if (e.index === 0) {
 		AG.settings.get('cloudSessionId') ? AG.loginController.logout() : AG.loginController.requireLogin();
 	}
 });
@@ -68,7 +37,7 @@ AG.loggedInUser.on('change', function(model) {
 });
 
 // $.loginBtn.addEventListener('click', function(e) {
-	// AG.settings.get('cloudSessionId') ? AG.loginController.logout() : AG.loginController.requireLogin();
+// AG.settings.get('cloudSessionId') ? AG.loginController.logout() : AG.loginController.requireLogin();
 // });
 function loginChangeHandler() {
 	// 최초에 이미 로그인 되어 있을 경우에 대한 처리
@@ -78,6 +47,7 @@ function loginChangeHandler() {
 		// $.resetClass($.loginBtn, 'beforeLogin');
 	}
 }
+
 loginChangeHandler();
 
 function showCamera() {
@@ -94,9 +64,11 @@ exports.setProperties = function() {
 	$.name.text = user.get('first_name');
 	$.profileImage.image = String.format("https://graph.facebook.com/%s/picture?width=%d&height=%d", fb_id, 140, 140);
 	// $.profileBannerImage.image = String.format("https://graph.facebook.com/%s?fields=cover", user.get('external_accounts')[0].external_id);
-	AG.facebook.requestWithGraphPath(fb_id, {fields: 'cover'} , "GET", function(e) {
+	AG.facebook.requestWithGraphPath(fb_id, {
+		fields : 'cover'
+	}, "GET", function(e) {
 		// alert(e);
-		if(e.success){
+		if (e.success) {
 			// alert(JSON.parse(e.result).cover.source);
 			$.profileBannerImage.image = JSON.parse(e.result).cover.source;
 		}
