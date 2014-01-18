16c8cd406657a9f2db2edf257a9844c42cdc25aa
Daehyeon Shin <dahini@gmail.com>
2014-01-18 22:25:14 +0900

    BOG-101 #resolve 디폴트 음식사진. 이것도 프로필픽쳐와 마찬가지로 defaultImage만 적용하는게 안먹혀서 image에도 셋팅했음. 원래 그림보다 어둡게 나오는데 왜 그런건지 일단 넘어감.
    Former-commit-id: 87950ea591bc568e677072bafee38a9679d23ac9

--- app/lib/colors.js
+++ app/lib/colors.js
@@ -23,7 +23,7 @@ exports = {
 		magenta : [ { color: '#ef4db6', offset: 0.0}, { color: '#c643fc', offset: 1.0 } ],
 		black : [ { color: '#4a4a4a', offset: 0.0}, { color: '#2b2b2b', offset: 1.0 } ],
 		silver : [ { color: '#dbddde', offset: 0.0}, { color: '#898c90', offset: 1.0 } ],
-		white : [ { color: '#000', offset: 0.0}, { color: '#FEFEFE', offset: 1.0 } ]
+		white : [ { color: '#FcFEFE', offset: 0.0}, { color: '#FEFEFE', offset: 1.0 } ]
 	},
 	
 	/*
