0f8c3537e2e2c9e82d56e2678c8976462c545594
Daehyeon Shin <dahini@gmail.com>
2014-03-05 10:28:39 +0900

    BOG-166 디바이스에서는 hiddenProfileOnLoad()가 전부 호출이 잘
    Former-commit-id: 2110308ec1b323b4d94f56688c333b497be73016

--- app/controllers/postItemTemplate.js
+++ app/controllers/postItemTemplate.js
@@ -3,7 +3,7 @@ function hiddenProfileOnLoad(){
 		// return proxy.bindId === 'profileImage';
 	// }).image = this.image;
 	this.parent.children[6].image = this.image;
-	this.parent.children[6].mask = 'images/profileMask.png';
+	// this.parent.children[6].mask = 'images/profileMask.png';
 	// this.parent.children[6].mode = Ti.UI.iOS.BLEND_MODE_SOURCE_IN;
 	//TODO : proxy찾는 하드코딩된 부분을 제거
 }
