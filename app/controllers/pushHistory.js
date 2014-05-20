f1c98c1b3ae6cfdba91792407ee765acde0e5d4e
Daehyeon Shin <dahini@gmail.com>
2014-05-20 20:32:16 +0900

    처음 로그인 했을때 participate_ids가 제대로 설정 안되는 문제 해결. 로그아웃 했을때 기존 데이터 삭제.
    Former-commit-id: 9e45d3337f8fd30fb41b3d40e19dff5456c70c53

--- app/controllers/pushHistory.js
+++ app/controllers/pushHistory.js
@@ -1,32 +1,28 @@
 var args = arguments[0] || {};
-
-
 var chatCol = Alloy.createCollection('chat');
 
-//따로 채팅기능 구현이 있을 수 있으므로 push용 구분을 위해 system용 id를 같이 넣어준다.
-function addSystemPushId(arr){
-	arr.push((ENV_DEV || ENV_TEST)?"5345c10f891fdf43ba114cec":"5345c244891fdf43ba114e39");
-	return arr;
+function setDefaultFetchData(){
+	chatCol.defaultFetchData = {
+		participate_ids : [
+			//따로 채팅기능 구현이 있을 수 있으므로 push용 구분을 위해 system용 id를 같이 넣어준다.
+			(ENV_DEV || ENV_TEST)?"5345c10f891fdf43ba114cec":"5345c244891fdf43ba114e39",
+			AG.loggedInUser.get('id') ].join(','),
+		where : {
+			updated_at: { '$gt': AG.moment().subtract('days', 7).toISOString() }, // 최근 7일
+			from_id: { "$ne": AG.loggedInUser.get('id')}	// 자신이 포스트한 것이 아닌것만
+		}
+	}; // 상속할때 where에 추가하고 싶으면 여기에 지정
 }
-
-
-chatCol.defaultFetchData = {
-	participate_ids : addSystemPushId([AG.loggedInUser.get('id')]).join(','),
-	where : {
-		updated_at: { '$gt': AG.moment().subtract('days', 7).toISOString() }, // 최근 7일
-		from_id: { "$ne": AG.loggedInUser.get('id')}	// 자신이 포스트한 것이 아닌것만
-	}
-}; // 상속할때 where에 추가하고 싶으면 여기에 지정
+setDefaultFetchData();
 
 function onChangeLoginUser(){
-	if(chatCol.defaultFetchData){
-		chatCol.defaultFetchData.participate_ids = addSystemPushId([AG.loggedInUser.get('id')]).join(',');
-	};
+	setDefaultFetchData();
+	
+	if(!AG.settings.get('cloudSessionId')){
+		chatCol.reset();
+	}	
 }
-
 AG.settings.on('change:cloudSessionId', onChangeLoginUser);
-
-
 
 $.listViewC.setCollection(chatCol);
 
@@ -34,13 +30,13 @@ $.listViewC.setTemplateControls([
 	'pushItemTemplate'
 ]);
 
-$.getView().addEventListener('focus', function(e) {
-	chatCol.fetch();
-});
-
 $.listViewC.on('itemclick',_.throttle(function(e){
 	var data = JSON.parse(e.model.get('message'));
 	AG.utils.openController(AG.mainTabGroup.activeTab, 'postDetail', {
 		post_id : data.post_id
 	});
 },1000));
+
+$.getView().addEventListener('focus', function(e) {
+	chatCol.fetch();
+});
