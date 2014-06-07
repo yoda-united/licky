c1d561f8f4d155af19b26dcd2dd393c8c7c466b1
Daehyeon Shin <dahini@gmail.com>
2014-06-08 01:09:10 +0900

    사진 또 수정
    Former-commit-id: 7b340b9d5a6a4e45b683192bcfcd3b68557d8ae2

--- app/controllers/walkthrough.js
+++ app/controllers/walkthrough.js
@@ -1,12 +1,29 @@
 var args = arguments[0] || {};
 
-
 $.step03Button.addEventListener('click', function(){
 	AG.settings.save('isWalkthroughMaster', true);
 	$.walkthrough.close();
 });
-
+$.step00.addEventListener('click', function(){
+	$.step00.animate({
+			opacity: 0.0,
+			top: Ti.Platform.displayCaps.platformHeight,
+			delay: 0,
+			curve: Ti.UI.ANIMATION_CURVE_EASE_OUT,
+			duration: 400
+		});
+	$.step00.removeEventListener('click', arguments.callee);
+});
 $.scrollableView.addEventListener('scrollend', function(e){
+	// if( e.currentPage == 1){
+		// var t1 = Ti.UI.create3DMatrix();
+		  // t1 = t1.translate(0, 100, 200);
+		  // t1.m34 = 1.0/-90;
+		// $.welcomeLabel.animate({
+			// transform: t1
+			// transition: Ti.UI.iPhone.AnimationStyle.CURL_UP 
+		// });
+	// }
 	// 3 페이지 일때 
 	if( e.currentPage == 2	) {
 		// $.step03Button.setVisible(true);
