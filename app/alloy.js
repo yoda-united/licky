ce2bca701cfc32302cdf87204afd6f9924173033
Jong Eun Lee <yomybaby@yomybaby.com>
2014-07-03 08:48:22 +0900

    시작하자마자 push 물어보는 코드 삭제
    Former-commit-id: be138684b4c8f6ef0f8a5f4f1960854a0e228e3a

--- app/alloy.js
+++ app/alloy.js
@@ -82,35 +82,6 @@ AG.settings.fetch({
 	}
 });
 
-// push notification
-if( OS_IOS ){
-	// Ti.Network.unregisterForPushNotifications();
-	Ti.Network.registerForPushNotifications({
-		types: [
-			Ti.Network.NOTIFICATION_TYPE_BADGE,
-			Ti.Network.NOTIFICATION_TYPE_ALERT,
-			Ti.Network.NOTIFICATION_TYPE_SOUND
-		],
-		callback: function(e){
-			// alert("data: "+JSON.stringify(e.data) +"\n"+e.inBackground);
-			// 뱃지를 0으로 하는거를 무시하지 않으면 무한 반복 푸쉬 됨..
-			if(  e.data.badge === 0 ){
-				return;
-			}
-			AG.notifyController.push({
-				pushEvent: e
-			});
-		},
-		error: function(e){
-			Ti.API.info('register for pushnotification error');
-		},
-		success: function(e){
-			AG.settings.save("deviceToken", Ti.Network.getRemoteDeviceUUID() );
-			AG.loginController.subscribePushChannel();
-		}
-	});
-}
-
 AG.currentPosition = Alloy.Models.instance('currentPosition');
 AG.currentPosition.update();
 
