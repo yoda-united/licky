10a7b92a34e718e8e03a5fa89e773ab64f26f038
Jong Eun Lee <yomybaby@yomybaby.com>
2014-07-03 08:47:44 +0900

    allow Push 컨트롤러 스타일 변경
    Former-commit-id: ecc0a59a920fbcd19880f4322b55b6795482164d

--- app/controllers/allowPushDialog.js
+++ app/controllers/allowPushDialog.js
@@ -1,1 +1,51 @@
+/**
+ * - 글 작성 후 
+ * - 댓글 작성 후
+ * - 알림 탭에서
+ */
+
 var args = arguments[0] || {};
+
+function onClickCancel(){
+	$.getView().close();
+}
+
+function onClickPlease(){
+	// push notification
+	registerForPushNotifications(function(e){
+		alert(e);
+		$.getView().close();
+	});	
+}
+
+function registerForPushNotifications(cb){
+	if( OS_IOS ){
+		// Ti.Network.unregisterForPushNotifications();
+		Ti.Network.registerForPushNotifications({
+			types: [
+				Ti.Network.NOTIFICATION_TYPE_BADGE,
+				Ti.Network.NOTIFICATION_TYPE_ALERT,
+				Ti.Network.NOTIFICATION_TYPE_SOUND
+			],
+			callback: function(e){
+				// alert("data: "+JSON.stringify(e.data) +"\n"+e.inBackground);
+				// 뱃지를 0으로 하는거를 무시하지 않으면 무한 반복 푸쉬 됨..
+				if(  e.data.badge === 0 ){
+					return;
+				}
+				AG.notifyController.push({
+					pushEvent: e
+				});
+			},
+			error: function(e){
+				Ti.API.info('register for pushnotification error');
+			},
+			success: function(e){
+				_.isFunction(cb) && cb(e);
+				AG.settings.save("deviceToken", Ti.Network.getRemoteDeviceUUID() );
+				AG.loginController.subscribePushChannel();
+			}
+		});
+	}
+}
+exports.registerForPushNotifications = registerForPushNotifications;
