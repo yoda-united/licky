eb3544df3dd94819d653200038300c321e0de4a6
Jong Eun Lee <yomybaby@yomybaby.com>
2014-09-27 11:07:32 +0900

    BOG-274 BOG-283 #resolve google map ios module yomybaby/TiGMap 6b6cc3679b4e46f922e40385d84555a687fa0e7d
    Former-commit-id: 87570c6ba8d3b57f1f273883634cbed4bf8a7fe0

--- app/controllers/mapWindow.js
+++ app/controllers/mapWindow.js
@@ -3,31 +3,34 @@ var args = arguments[0] || {},
 
 var GoogleMapsClass, GoogleMaps;
 
-if(!GoogleMaps){
-	(function(){
-		GoogleMapsClass = require('GoogleMaps');
-		GoogleMaps = new GoogleMapsClass({
-			iOSKey: Ti.App.Properties.getString('google-map-ios-key')
-		});
-		var coord = postModel.get("custom_fields").coordinates;
-		var mapView = GoogleMaps.initMap({
-			latitude:coord[0][1],
-			longitude:coord[0][0],
-			zoom: 16, //15, 16이 적당해 보임
-			// width : 304,
-			// height : 119,
-			// top:0,
-			userLocation : (Ti.Geolocation.getLocationServicesAuthorization() == Ti.Geolocation.AUTHORIZATION_AUTHORIZED)?true:false
-		});
-		// $.mapWrap.setHeight(119);
-		$.mapWrap.add(mapView);
-		mapView.backgroundColor = 'white';
-		
-		var marker1 = GoogleMaps.createMarker({
-			latitude:coord[0][1],
-			longitude:coord[0][0],
-			image: 'images/flag4map' //png 붙이지 마시오. tishadow에서 보려면 파일 변경후 다시 appify해야함
-		});
-		GoogleMaps.addMarker(marker1);
-	})();
-}
+$.getView().addEventListener('open', function(e) {
+	
+	if(!GoogleMaps){
+		(function(){
+			GoogleMapsClass = require('GoogleMaps');
+			GoogleMaps = new GoogleMapsClass({
+				iOSKey: Ti.App.Properties.getString('google-map-ios-key')
+			});
+			var coord = postModel.get("custom_fields").coordinates;
+			var mapView = GoogleMaps.initMap({
+				latitude:coord[0][1],
+				longitude:coord[0][0],
+				zoom: 16, //15, 16이 적당해 보임
+				// width : 304,
+				// height : 119,
+				// top:0,
+				userLocation : (Ti.Geolocation.getLocationServicesAuthorization() == Ti.Geolocation.AUTHORIZATION_AUTHORIZED)?true:false
+			});
+			// $.mapWrap.setHeight(119);
+			$.mapWrap.add(mapView);
+			mapView.backgroundColor = 'white';
+			
+			var marker1 = GoogleMaps.createMarker({
+				latitude:coord[0][1],
+				longitude:coord[0][0],
+				image: 'images/flag4map' //png 붙이지 마시오. tishadow에서 보려면 파일 변경후 다시 appify해야함
+			});
+			GoogleMaps.addMarker(marker1);
+		})();
+	}
+});
